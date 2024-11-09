3f3706fc216ecc080866666b38708073cb21cb27
TrickyLeifa <date.epoch@gmail.com>
2024-11-09 01:35:45 +0100

    Formatted VPath source file

--- src/vpath.cpp
+++ src/vpath.cpp
@@ -1,16 +1,23 @@
 #include "vpath.h"
 
-
-
 VPath::VPath(const QString &str)
-  : QString(str)
+    : QString(str)
 {}
 
-const QString &VPath::toString() const { return *this; }
+const QString &VPath::toString() const
+{
+  return *this;
+}
 
-bool VPath::operator==(const VPath &str) const { return this->toString() == str.toString(); }
+bool VPath::operator==(const VPath &str) const
+{
+  return this->toString() == str.toString();
+}
 
-VPath VPath::operator+(const VPath &str) const { return VPath(this->toString() + str.toString()); }
+VPath VPath::operator+(const VPath &str) const
+{
+  return VPath(this->toString() + str.toString());
+}
 
 size_t qHash(const VPath &key, size_t seed)
 {
